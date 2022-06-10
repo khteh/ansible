8ca371c776a4140a588497deb6a19c28942fa6b9
Kok How, Teh <funcoolgeek@gmail.com>
2022-06-10 16:53:58 +0800

    Add ansible playbooks to view, start and stop AWS EC2 instances

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ansible-playbook --vault-id windows@vault-secret -i windwos.yml site.yml
 ansible-playbook --vault-id local@vault-secret -i localhost.yml local_playbook.yml
+ansible-playbook -i localhost.yml aws_ec2_info.yml
+ansible-playbook -i localhost.yml start_aws_ec2_playbook.yml --tags=launch_ec2
+ansible-playbook -i localhost.yml stop_aws_ec2_playbook.yml --tags=stop_all_ec2
